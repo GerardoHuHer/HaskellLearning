78df3217037033e41d2e07e61f5a5416baebc897
Gerardo Huerta Hernández <gerardohuerta1502@gmail.com>
2023-12-14 15:51:46 -0600

    Se añade Chapter4.hs

---
+++ Chapter4.hs
@@ -0,0 +1,30 @@
+ifEven myfunction x = if even x
+  then myfunction x
+    else x
+
+inc::Int -> Int
+inc n = n + 1
+
+double::Int -> Int
+double n = n * 2
+
+square::Int -> Int
+square n = n ^ 2
+
+cube::Int -> Int
+cube n = n ^ 3
+
+negative::Int -> Int
+negative n = n * (-1)
+
+-- ifEvenInc n = ifEven inc n
+ifEvenDouble = ifEven double
+ifEvenSquare = ifEven square
+ifEvenCube = ifEven cube
+ifEvenNegative = ifEven negative
+-- ifEvenLambdaCube = ifEven (\x -> x ^ 3)
+ifEvenLambdaCube = ifEven (^ 3)
+
+getIfEven f = (\x -> ifEven f x)
+
+ifEvenInc = getIfEven inc
