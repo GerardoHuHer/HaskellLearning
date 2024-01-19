eece622c721e91c67068fefcdeccaf8faa719f95
Gerardo Huerta Hernández <gerardohuerta1502@gmail.com>
2024-01-18 22:11:21 -0600

    Modificación

--- Chapter5.hs
+++ Chapter5.hs
@@ -1,4 +1,4 @@
--- This topic (list) is covered from page 69 to 
+-- This topic (list) is covered from page 69 to
 teams = ["red", "yellow", "orange", "blue", "purple"]
 list = [1,2,3]
 list2 = [[1,2],[3,4]]
