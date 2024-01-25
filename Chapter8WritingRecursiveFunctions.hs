50d54f88b3e95c373910381c2492b84d0a6295c2
Gerardo Huerta Hernández <gerardohuerta1502@gmail.com>
2024-01-25 14:05:46 -0600

    Continuación Capítulo 8

--- Chapter8WritingRecursiveFunctions.hs
+++ Chapter8WritingRecursiveFunctions.hs
@@ -1,4 +1,23 @@
-myDrop :: Int -> [a] -> [a] -> [a]
-myDrop 0 (x:xs) lista = reverse lista
-myDrop num (x:xs) lista
-    |
+myDrop 0 xs = xs
+myDrop n ys = case ys of
+    x:xs -> myDrop (x -1) xs
+    [] -> []
+
+    
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (x:xs) = 1 + myLength xs
+
+-- Crea una función recursiva que implemente take
+myTake :: Int -> [a] -> [a]
+myTake _ [] = []
+myTake 0 _ = []
+myTake n (x:xs) = x:rest
+    where rest = myTake (n - 1) xs
+
+-- Ejercicio de una conjetura de cola
+conjeturaCola :: Int -> [Int] -> [Int]
+conjeturaCola 1 lista = reverse (1:lista)
+conjeturaCola num lista 
+        | even num = conjeturaCola (div num 2) (num:lista)
+        | odd num = conjeturaCola (3 *num + 1) (num:lista)
